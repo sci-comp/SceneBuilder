6b5d92574cf4a6213773838e66a445696970e787
Paul <arr.explore@gmail.com>
2025-01-20 10:22:05 -0500

    Rewrite scene_builder_plugin.gd
    https://github.com/sci-comp/SceneBuilder/issues/25

--- addons/SceneBuilder/scene_builder_plugin.gd
+++ addons/SceneBuilder/scene_builder_plugin.gd
@@ -1,47 +1,65 @@
 @tool
 extends EditorPlugin
 
+const BASE_PATHS = [
+	"res://addons/SceneBuilder/",
+	"res://addons/SceneBuilder/addons/SceneBuilder/"
+]
+
 var scene_builder_dock : SceneBuilderDock
 var scene_builder_commands : SceneBuilderCommands
 var scene_builder_config : SceneBuilderConfig
 
-func _enter_tree():
-	if (ResourceLoader.exists("res://addons/SceneBuilder/scene_builder_dock.gd") and
-		ResourceLoader.exists("res://addons/SceneBuilder/scene_builder_commands.gd") and
-		ResourceLoader.exists("res://addons/SceneBuilder/scene_builder_config.tres")
-		):
+func _enter_tree() -> void:
+	var base_path = _find_valid_base_path()
+	if not base_path:
+		printerr("SceneBuilder addon files not found in expected locations.")
+		return
 
-		scene_builder_dock = load("res://addons/SceneBuilder/scene_builder_dock.gd").new()
-		scene_builder_commands = load("res://addons/SceneBuilder/scene_builder_commands.gd").new()
-		scene_builder_config = load("res://addons/SceneBuilder/scene_builder_config.tres") as SceneBuilderConfig
-
-	# Recursive directories will exist when installing from a submodule
-	elif (ResourceLoader.exists("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_dock.gd") and
-		ResourceLoader.exists("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_commands.gd") and
-		ResourceLoader.exists("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_config.tres")
-		):
-
-		scene_builder_dock = load("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_dock.gd").new()
-		scene_builder_commands = load("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_commands.gd").new()
-		scene_builder_config = load("res://addons/SceneBuilder/addons/SceneBuilder/scene_builder_config.tres")
-
-	else:
-		printerr("scene_builder_dock.gd, scene_builder_commands.gd, or scene_builder_config.gd was not found.")
+	if not _load_or_create_components(base_path):
 		return
 
 	scene_builder_dock.update_config(scene_builder_config)
 	scene_builder_commands.update_config(scene_builder_config)
-
+	
 	add_child(scene_builder_commands)
 	add_child(scene_builder_dock)
 
+func _find_valid_base_path() -> String:
+	for path in BASE_PATHS:
+		if ResourceLoader.exists(path + "scene_builder_dock.gd") and \
+		   ResourceLoader.exists(path + "scene_builder_commands.gd") and \
+		   ResourceLoader.exists(path + "scene_builder_config.gd"):
+			return path
+	return ""
+
+func _load_or_create_components(base_path: String) -> bool:
+	scene_builder_dock = load(base_path + "scene_builder_dock.gd").new()
+	scene_builder_commands = load(base_path + "scene_builder_commands.gd").new()
+	
+	var config_path = base_path + "scene_builder_config.tres"
+	var config_script = load(base_path + "scene_builder_config.gd")
+	
+	if ResourceLoader.exists(config_path):
+		print("[SceneBuilderPlugin] Configuration file found")
+		scene_builder_config = load(config_path)
+	else:
+		scene_builder_config = config_script.new()
+		var err = ResourceSaver.save(scene_builder_config, config_path)
+		print("[SceneBuilderPlugin] Configuration file not found, a new one has been saved to: ", config_path)
+		if err != OK:
+			printerr("[SceneBuilderPlugin] Failed to save new config file")
+			return false
+	return true
+
 func _exit_tree():
-	scene_builder_commands.queue_free()
-	scene_builder_dock.queue_free()
+	if scene_builder_commands:
+		scene_builder_commands.queue_free()
+	if scene_builder_dock:
+		scene_builder_dock.queue_free()
 
 func _handles(object):
 	return object is Node3D
 
 func _forward_3d_gui_input(camera : Camera3D, event : InputEvent) -> AfterGUIInput:
-	var _return : AfterGUIInput = scene_builder_dock.forward_3d_gui_input(camera, event)
-	return _return
+	return scene_builder_dock.forward_3d_gui_input(camera, event)
