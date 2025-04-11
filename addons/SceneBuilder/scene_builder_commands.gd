58b43a2e8b196c44b3024de308ff546a3b7ef60d
Paul <arr.explore@gmail.com>
2025-04-11 17:13:24 -0400

    Removing a command that only works for Mono builds

--- addons/SceneBuilder/scene_builder_commands.gd
+++ addons/SceneBuilder/scene_builder_commands.gd
@@ -13,7 +13,6 @@ enum SceneCommands
 	create_audio_stream_player_3d = 9,
 	create_scene_builder_items = 10,
 	create_standard_material_3d = 12,
-	find_mismatched_types = 15,
 	fix_negative_scaling = 20,
 	instantiate_in_a_row_1 = 32,
 	instantiate_in_a_row_2 = 33,
@@ -43,8 +42,6 @@ func _unhandled_input(event: InputEvent):
 						create_scene_builder_items()
 					config.create_standard_material_3d:
 						create_standard_material_3d()
-					config.find_mismatched_types:
-						find_mismatched_types()
 					config.instantiate_in_a_row_1:
 						instantiate_in_a_row(1)
 					config.instantiate_in_a_row_2:
@@ -79,7 +76,6 @@ func _enter_tree():
 	submenu_scene.add_item("Create audio stream player 3d", SceneCommands.create_audio_stream_player_3d)
 	submenu_scene.add_item("Create scene builder items", SceneCommands.create_scene_builder_items)
 	submenu_scene.add_item("Create StandardMaterial3D", SceneCommands.create_standard_material_3d)
-	submenu_scene.add_item("Find mismatched types", SceneCommands.find_mismatched_types)
 	submenu_scene.add_item("Fix negative scaling", SceneCommands.fix_negative_scaling)
 	submenu_scene.add_item("Instantiate selected paths in a row (1m)", SceneCommands.instantiate_in_a_row_1)
 	submenu_scene.add_item("Instantiate selected paths in a row (5m)", SceneCommands.instantiate_in_a_row_2)
@@ -107,8 +103,6 @@ func _on_scene_submenu_item_selected(id: int):
 			create_scene_builder_items()
 		SceneCommands.create_standard_material_3d:
 			create_standard_material_3d()
-		SceneCommands.find_mismatched_types:
-			find_mismatched_types()
 		SceneCommands.fix_negative_scaling:
 			fix_negative_scaling()
 		SceneCommands.instantiate_in_a_row_1:
@@ -154,10 +148,6 @@ func create_standard_material_3d():
 
 func create_audio_stream_player_3d():
 	var _instance = preload("./Commands/create_audio_stream_player_3d.gd").new()
-	_instance.execute()
-
-func find_mismatched_types():
-	var _instance = preload("./Commands/find_mismatched_types.gd").new()
 	_instance.execute()
 
 func fix_negative_scaling():
